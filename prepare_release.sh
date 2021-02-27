c8088647c1b7d64bcef932f31dcf2dbc6da881f4
Suraj Shrestha <syuraj@gmail.com>
2021-02-27 00:25:43 -0500

    new logo

--- prepare_release.sh
+++ prepare_release.sh
@@ -3,6 +3,7 @@
 PACKAGE_NAME="com.siristechnology.hypergrowth"
 APP_NAME="HyperGrowth"
 TIME_STAMP=$(date +%s)
+IC_LAUNCHER_BACKGROUND="#E7F3FF"
 BUILD_GRADLE=./android/app/build.gradle
 ANDROID_STRINGS_FILE=./android/app/src/main/res/values/strings.xml
 ANDROID_MANIFEST_FILE=./android/app/src/main/AndroidManifest.xml
@@ -12,8 +13,7 @@ sed -i '' -e 's/applicationId "[^"]*"/applicationId "'$PACKAGE_NAME'"/' $BUILD_G
 sed -i '' -e 's/versionCode .*$/versionCode '$TIME_STAMP'/' $BUILD_GRADLE
 sed -i '' -e 's/"app_name">[^<]*</"app_name">'$APP_NAME'</' $ANDROID_STRINGS_FILE
 
-sed -i '' -e 's/"@mipmap\/ic_launcher"/"@mipmap\/ic_launcher_prod"/' $ANDROID_MANIFEST_FILE
-
+sed -i '' -e 's/"ic_launcher_background">[^<]*</"ic_launcher_background">'$IC_LAUNCHER_BACKGROUND'</' $IC_LAUNCHER_BACKGROUND_FILE
 
 mv .env.release .env
 mv ./android/app/google-services.json.prod ./android/app/google-services.json
