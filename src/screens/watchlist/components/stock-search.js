388a6e70347a0e7c8cbe36e20cad28356e454120
Suraj Shrestha <syuraj@gmail.com>
2021-04-03 19:39:37 -0400

    add searched stock to watchlist

--- src/screens/watchlist/components/stock-search.js
+++ src/screens/watchlist/components/stock-search.js
@@ -1,14 +1,13 @@
 import React, { useEffect } from 'react'
 import { Autocomplete, AutocompleteItem } from '@ui-kitten/components'
-import gql from 'graphql-tag'
-import { useLazyQuery } from '@apollo/client'
+import { gql, useLazyQuery, useMutation } from '@apollo/client'
 
 const StockSearch = () => {
 	const [searchTerm, setSearchTerm] = React.useState('')
-
 	const [searchStocks, { data }] = useLazyQuery(SEARCH_STOCKS_QUERY, {
 		variables: { searchTerm },
 	})
+	const [addStock, { data2 }] = useMutation(ADD_STOCK_MUTATION)
 
 	useEffect(() => {
 		let isMounted = true
@@ -23,11 +22,14 @@ const StockSearch = () => {
 
 	const onSelect = (index: number) => {
 		setSearchTerm(data.searchStocks[index].symbol)
+		addStock({ variables: { symbol: data.searchStocks[index].symbol } })
 	}
 
 	const onChangeText = (nextQuery) => {
 		setSearchTerm(nextQuery)
 	}
+
+	console.log('printing data2', data2)
 
 	const renderOption = (item, index) => <AutocompleteItem key={index} title={item.symbol + ' | ' + item.company} />
 
@@ -38,11 +40,20 @@ const StockSearch = () => {
 	)
 }
 
-export const SEARCH_STOCKS_QUERY = gql`
+const SEARCH_STOCKS_QUERY = gql`
 	query searchStocksQuery($searchTerm: String!) {
 		searchStocks(searchTerm: $searchTerm) {
 			symbol
 			company
+		}
+	}
+`
+
+const ADD_STOCK_MUTATION = gql`
+	mutation addStock($symbol: String!) {
+		addStockToWatchList(symbol: $symbol) {
+			success
+			message
 		}
 	}
 `
