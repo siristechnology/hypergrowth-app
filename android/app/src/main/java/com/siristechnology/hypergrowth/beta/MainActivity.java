5a31f269abf2d357a41b5b7eb54d0ef8fe544ed0
Suraj Shrestha <syuraj@gmail.com>
2021-05-08 15:08:57 -0400

    bug: app crash on start

--- android/app/src/main/java/com/siristechnology/hypergrowth/beta/MainActivity.java
+++ android/app/src/main/java/com/siristechnology/hypergrowth/beta/MainActivity.java
@@ -13,10 +13,8 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this); // <- display the generated bootsplash.xml drawable over our MainActivity
+    super.onCreate(null);
   }
-
 
   @Override
   protected String getMainComponentName() {
